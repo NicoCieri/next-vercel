27f7745d9693a71fe4cdf14d8c5974c48689bb62
Nicolás Cieri <nico.cieri13@gmail.com>
2023-03-31 15:35:24 -0300

    Pre-deployment Vercel

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { CSSProperties } from "react";
+
+const style: CSSProperties = {
+  color: "#0070f3",
+  textDecoration: "underline",
+};
+
+interface ActiveLinkProps {
+  text: string;
+  href: string;
+}
+
+export const ActiveLink = ({ text, href }: ActiveLinkProps) => {
+  const { asPath } = useRouter();
+
+  return (
+    <Link href={href}>
+      <a style={asPath === href ? style : undefined}>{text}</a>
+    </Link>
+  );
+};
